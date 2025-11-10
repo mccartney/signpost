127a978aa8747a086407048425158a453b7483b0
Greg Oledzki <grzegon@poczta.onet.pl>
2025-11-10 17:35:49 +0100

    Scala 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,16 +11,21 @@ dependencies {
     implementation(libs.scala.library)
     implementation(libs.guava)
 
-    testImplementation(libs.junit)
-    testImplementation(libs.scalatest.v2.v13)
-    testImplementation(libs.junit.v4.v13.v2.v13)
-
-    testRuntimeOnly(libs.scala.xml.v2.v13)
+    testImplementation(libs.scalatest)
+    testRuntimeOnly(libs.scala.xml)
+    testRuntimeOnly("org.scala-lang.modules:scala-xml_3:2.3.0")
+    testRuntimeOnly("org.scalatestplus:junit-5-10_3:3.2.19.0")
 }
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform {
+        includeEngines("scalatest")
     }
 }
 
