1e260eed9b0a4ca6e2625d0616ae4d26608f7793
Greg Oledzki <grzegon@poczta.onet.pl>
2025-11-11 07:57:33 +0100

    First solver rule

--- app/src/main/scala/pl/waw/oledzki/signpost/Solver.scala
+++ app/src/main/scala/pl/waw/oledzki/signpost/Solver.scala
@@ -11,6 +11,30 @@ class Solver {
   }
 
   def applyRules(board: Board): Board = {
-    board
+    val rules = List[Rule](
+      new ConnectedCellsHaveConsecutiveNumber()
+    )
+    Function.chain(rules.map(r => r.evaluate))(board)
+  }
+}
+
+abstract class Rule {
+  def evaluate(board: Board): Board
+}
+
+class ConnectedCellsHaveConsecutiveNumber extends Rule {
+  override def evaluate(board: Board): Board = {
+    board.connections.collectFirst { case ((x1, y1), (x2, y2)) =>
+        val cell1 = board.cells((x1, y1))
+        val cell2 = board.cells((x2, y2))
+        (cell1.visitingNumber, cell2.visitingNumber) match {
+          case (Some(num1), None) =>
+            Some(board.copy(cells = board.cells + ((x2, y2) -> cell2.copy(visitingNumber = Some(num1 + 1)))))
+          case (None, Some(num2)) =>
+            Some(board.copy(cells = board.cells + ((x1, y1) -> cell1.copy(visitingNumber = Some(num2 - 1)))))
+          case _ =>
+            None
+        }
+      }.flatten.getOrElse(board)
   }
 }
